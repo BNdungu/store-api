e3479c657273dc13d2ad6ffb44fe56d8d0e013e2
BNdungu <bryannganga909@gmail.com>
2024-01-03 08:50:22 +0300

    app

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,12 @@
-console.log('04 Store API')
+require('dotenv').config
+const express = require('express')
+
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+    console.log(`Server is Listening at Port ${port}`)
+}) 
