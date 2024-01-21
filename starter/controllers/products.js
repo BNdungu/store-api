9f155ac56bc8c6c64e4da09adc9f4ff5e621df4c
BNdungu <bryannganga909@gmail.com>
2024-01-21 09:15:21 +0300

    controller

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,9 +1,37 @@
+const model = require('../models/product')
+
 const getAllProductsStatic = async (req,res) => {
-    res.status(200).json({msg: "get all products static"})
+    const search = 'ab'
+    const products = await model.find({}).sort('-name price')
+    res.status(200).json({products,nbHits: products.length})
 }
 
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg: "get all products"})
+    const {featured,company,name} = req.query
+    console.log(featured)
+    const queryObj = {}
+
+    if (featured){
+        queryObj.featured = featured === 'true' ? true : false
+    }
+
+    if (company){
+        queryObj.company = company
+    }
+
+    if (name){
+        queryObj.name = { $regex: name, $options: 'i'}
+    }
+
+    console.log(queryObj)
+    const products = await model.find(queryObj)
+    res.status(200).json({
+        status: 'success',
+        result: products.length,
+        data: {
+            products
+        }
+    })
 }
 
 module.exports = {
